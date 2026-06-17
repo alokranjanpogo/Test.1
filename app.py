41e5a9f449fc8f92880dd0b52cdd085ca5690b81
alokranjanpogo <alokranjan18april@gmail.com>
2026-06-17 10:39:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ from ultralytics import YOLO
 st.success("OpenCV Loaded")
 st.success(f"OpenCV Version: {cv2.__version__}")
 
-model = YOLO("yolov8n.pt")
+model = YOLO("best.pt")
 
 st.success("YOLO Loaded Successfully")
